495c58b3f4a352b185938636a26f9a15bb4e325f
Niroyan Kugaruban <niroyan.kugaruban@anjn.com.au>
2022-05-26 11:25:01 +0530

    ConditionalRendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const one = function show1() {
+    document.getElementById("div1").style.display = "none";
+  };
+  const two = function show2() {
+    document.getElementById("div1").style.display = "block";
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>How many check boxes do you want when clicked on a radio button?</p>
+      <input type="radio" name="tab" value="igotnone" onClick={one} />
+      None
+      <input type="radio" name="tab" value="igottwo" onClick={two} />
+      Two
+      <div id="div1" className="hide">
+        <hr />
+        <p>Okay Cool! Here are those two...</p>
+        <input type="checkbox" value="Yes" name="one" />
+        One &nbsp;
+        <input type="checkbox" value="Yes" name="two" />
+        Two
+      </div>
     </div>
   );
 }
